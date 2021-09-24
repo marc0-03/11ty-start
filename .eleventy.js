37cd79efaf91656d9b33ad51f58fc17f654651e1
Marc0-03 <marcusnylund44@gmail.com>
2021-09-24 12:27:39 +0200

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = function(eleventyConfig) {
+
+    eleventyConfig.addWatchTarget("./src/sass/");
+
+    eleventyConfig.addPassthroughCopy("./src/sass/");
+    
+    return {
+        dir: {
+            input: "src",
+            output: "public",
+        },
+    };
+};
